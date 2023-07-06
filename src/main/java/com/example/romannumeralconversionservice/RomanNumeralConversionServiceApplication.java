3aa87fd881056216e93886fdf1c2fcff3da961f1
ligint <ligin.thiya@gmail.com>
2023-07-06 16:44:59 -0700

    Initialize spring boot service

---
+++ src/main/java/com/example/romannumeralconversionservice/RomanNumeralConversionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.romannumeralconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanNumeralConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomanNumeralConversionServiceApplication.class, args);
+	}
+
+}
