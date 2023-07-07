3441ea7ea66fc61065cef9c98ae907a50ac73e7c
ligint <ligin.thiya@gmail.com>
2023-07-06 21:41:35 -0700

    Update Unit Tests to handle response entity

--- src/test/java/com/example/romannumeralconversionservice/RomanNumeralUnitTests.java
+++ src/test/java/com/example/romannumeralconversionservice/RomanNumeralUnitTests.java
@@ -4,8 +4,8 @@ import com.example.romannumeralconversionservice.controller.RomanNumeralControll
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
 
 public class RomanNumeralUnitTests {
 
@@ -14,35 +14,28 @@ public class RomanNumeralUnitTests {
     @Test
     @DisplayName("Valid Tests for Integer to Roman Conversion")
     public void testConversionToRoman_ValidNumber(){
-        Assertions.assertEquals("I", cont.romanNumeral(1).toString());
-        Assertions.assertEquals("V", cont.romanNumeral(5).toString());
-        Assertions.assertEquals("VIII", cont.romanNumeral(8).toString());
-        Assertions.assertEquals("X", cont.romanNumeral(10).toString());
-        Assertions.assertEquals("XII", cont.romanNumeral(12).toString());
-        Assertions.assertEquals("L", cont.romanNumeral(50).toString());
-        Assertions.assertEquals("LV", cont.romanNumeral(55).toString());
-        Assertions.assertEquals("C", cont.romanNumeral(100).toString());
-        Assertions.assertEquals("CV", cont.romanNumeral(105).toString());
-        Assertions.assertEquals("CD", cont.romanNumeral(400).toString());
-        Assertions.assertEquals("D", cont.romanNumeral(500).toString());
-        Assertions.assertEquals("MMM", cont.romanNumeral(3000).toString());
-        Assertions.assertEquals("MMMCMXCIX", cont.romanNumeral(3999).toString());
+        Assertions.assertEquals("{\"input\":\"1\",\"output\":\"I\"}", cont.romanNumeral(1).getBody());
+        Assertions.assertEquals("{\"input\":\"5\",\"output\":\"V\"}", cont.romanNumeral(5).getBody());
+        Assertions.assertEquals("{\"input\":\"8\",\"output\":\"VIII\"}", cont.romanNumeral(8).getBody());
+        Assertions.assertEquals("{\"input\":\"10\",\"output\":\"X\"}", cont.romanNumeral(10).getBody());
+        Assertions.assertEquals("{\"input\":\"12\",\"output\":\"XII\"}", cont.romanNumeral(12).getBody());
+        Assertions.assertEquals("{\"input\":\"50\",\"output\":\"L\"}", cont.romanNumeral(50).getBody());
+        Assertions.assertEquals("{\"input\":\"55\",\"output\":\"LV\"}", cont.romanNumeral(55).getBody());
+        Assertions.assertEquals("{\"input\":\"100\",\"output\":\"C\"}", cont.romanNumeral(100).getBody());
+        Assertions.assertEquals("{\"input\":\"105\",\"output\":\"CV\"}", cont.romanNumeral(105).getBody());
+        Assertions.assertEquals("{\"input\":\"400\",\"output\":\"CD\"}", cont.romanNumeral(400).getBody());
+        Assertions.assertEquals("{\"input\":\"500\",\"output\":\"D\"}", cont.romanNumeral(500).getBody());
+        Assertions.assertEquals("{\"input\":\"3000\",\"output\":\"MMM\"}", cont.romanNumeral(3000).getBody());
+        Assertions.assertEquals("{\"input\":\"3999\",\"output\":\"MMMCMXCIX\"}", cont.romanNumeral(3999).getBody());
     }
 
     @Test
     @DisplayName("In-Valid Tests for Integer to Roman Conversion")
     public void testConversionToRoman_InvalidNumber(){
-        Assertions.assertThrows(ResponseStatusException.class, ()-> {
-           cont.romanNumeral(0);
-        });
-        Assertions.assertThrows(ResponseStatusException.class, ()-> {
-            cont.romanNumeral(-10);
-        });
-        Assertions.assertThrows(ResponseStatusException.class, ()-> {
-            cont.romanNumeral(-200);
-        });
-        Assertions.assertThrows(ResponseStatusException.class, ()-> {
-            cont.romanNumeral(5999);
-        });
+
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, cont.romanNumeral(0).getStatusCode());
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, cont.romanNumeral(-10).getStatusCode());
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, cont.romanNumeral(-200).getStatusCode());
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, cont.romanNumeral(5999).getStatusCode());
     }
 }
