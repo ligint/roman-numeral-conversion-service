733e8ff234aa6dce0b43583e6ed66c6ca535ccb8
ligint <ligin.thiya@gmail.com>
2023-07-06 21:39:20 -0700

    Update Model logic to override toString method

--- src/main/java/com/example/romannumeralconversionservice/model/RomanNumeralModel.java
+++ src/main/java/com/example/romannumeralconversionservice/model/RomanNumeralModel.java
@@ -5,13 +5,19 @@ import java.util.TreeMap;
 
 public class RomanNumeralModel {
 
-    private  final String romanValue;
     private final Integer number;
+    private  final String romanValue;
 
+    /** Create TreeMap for storing the Roman Numerals and their Integer Value **/
     private final static TreeMap<Integer, String > romanValueMap = new TreeMap<>();
-    // reference: https://en.wikipedia.org/wiki/Roman_numerals
 
     static {
+        /**
+         * Initialize the TreeMap with Roman numerals
+         * and their corresponding integer values
+         * reference: https://en.wikipedia.org/wiki/Roman_numerals
+         * **/
+
         romanValueMap.put(1000, "M");
         romanValueMap.put(900, "CM");
         romanValueMap.put(500, "D");
@@ -33,25 +39,54 @@ public class RomanNumeralModel {
         this.romanValue = convertToRoman(number);
     }
 
+
     public Integer getNumber() {
         return this.number;
     }
-    public String getRomanValue(){return  this.romanValue;}
 
-    public String toString(){return  this.romanValue;}
+    public String getRomanValue(){
+        return  this.romanValue;
+    }
 
 
+    /**
+     * Override toString() Method since output is expected in below format
+     * {
+     * “input” : “1”,
+     * “output” : “I”
+     * }
+     */
+    @Override
+    public String toString(){
+       String input = this.getNumber().toString();
+       String output = this.romanValue;
 
+       return String.format("{\"input\":\"%s\",\"output\":\"%s\"}", input,output);
+    }
+
+
+    /**
+     * convertToRoman()
+     * Convert the number to Roman numeral.
+     *
+     * @param  Integer value between 1 to 3999.
+     *
+     * @return  Roman Numeral in String format.
+    **/
     private String convertToRoman(int number){
 
         StringBuilder romanNumeral = new StringBuilder();
-        for (int key: romanValueMap.descendingKeySet()){
-            while (number >= key){
-                romanNumeral.append(romanValueMap.get(key));
-                number -= key;
+
+        for (Map.Entry<Integer, String> entry: romanValueMap.descendingMap().entrySet()){
+
+            int value = entry.getKey();
+            String roman = entry.getValue();
+
+            while (number >= value){
+                romanNumeral.append(roman);
+                number -= value;
             }
         }
-
         return romanNumeral.toString();
     }
 }
